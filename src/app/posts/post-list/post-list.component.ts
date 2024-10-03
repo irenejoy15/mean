50aa594b1868722a3ca1a80ab7689f65f15c862c
irenejoy15 <34486370+irenejoy15@users.noreply.github.com>
2024-10-03 17:00:37 +0800

    Calling Get Post

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import {Component,Input} from '@angular/core'
+import {Component,Input, OnDestroy, OnInit} from '@angular/core'
+import { Subscription } from 'rxjs';
 import {Post} from '../post.model';
 import { PostsService } from '../post.service';
 // DECORATOR
@@ -7,13 +8,26 @@ import { PostsService } from '../post.service';
     templateUrl: './post-list.component.html',
     styleUrls: ['./post-list.component.css'],
 })
-export class PostListComponent{
+export class PostListComponent implements OnInit, OnDestroy{
 //    posts = [
 //     {title: 'First Post', content: 'This is a first post'},
 //     {title: 'Second Post', content: 'This is a Second post'},
 //     {title: 'Third Post', content: 'This is a Third post'},
 //    ];
-   @Input() posts: Post[] = [];
-
+   posts: Post[] = [];
+   private postsSub : Subscription;
+   
    constructor(public postsService: PostsService) {}
+
+   ngOnInit(){
+       this.posts = this.postsService.getPosts();
+       this.postsSub = this.postsService.getPostUpdateListener()
+        .subscribe((posts: Post[])=>{
+            this.posts = posts;
+        });
+   }
+
+   ngOnDestroy() {
+    this.postsSub.unsubscribe();
+   }
 }
