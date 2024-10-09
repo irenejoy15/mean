28c5421cede5001b1662be261ca8b2ceab4e485d
irenejoy15 <34486370+irenejoy15@users.noreply.github.com>
2024-10-09 10:22:21 +0800

    DELETE POST

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 import {Post} from './post.model'
+import {map} from 'rxjs/operators'
 
 @Injectable({providedIn: 'root'})
 export class PostsService{
@@ -11,10 +12,32 @@ export class PostsService{
     constructor(private http: HttpClient){}
 
     getPosts(){
-        // http://localhost:82/mean-backend/public/api/posts
-        this.http.get<{message: string, posts:Post[]}>('http://localhost:3000/api/posts')
-        .subscribe((postData)=>{
-            this.posts = postData.posts;
+        // this.http.get<{message: string, posts:any[]}>('http://localhost:82/mean-backend/public/api/posts')
+        // .pipe(map((postData)=>{
+        //     return postData.posts.map(post=>{
+        //         return{
+        //             title: post.title,
+        //             content: post.content,
+        //             id: post.id
+        //         };
+        //     });
+        // }))
+        // .subscribe((transformedPosts)=>{
+        //     this.posts = transformedPosts;
+        //     this.postsUpdated.next([...this.posts])
+        // });
+        this.http.get<{message: string, posts:any[]}>('http://localhost:3000/api/posts')
+        .pipe(map((postData)=>{
+            return postData.posts.map(post=>{
+                return{
+                    title: post.title,
+                    content: post.content,
+                    id: post._id
+                };
+            });
+        }))
+        .subscribe((transformedPosts)=>{
+            this.posts = transformedPosts;
             this.postsUpdated.next([...this.posts])
         });
     }
@@ -33,6 +56,15 @@ export class PostsService{
             console.log(responseData.message);
             this.posts.push(post)
             this.postsUpdated.next([...this.posts]);
+        });
+    }
+
+    deletePost(postId: string){
+        // // http://localhost:82/mean-backend/public/api/posts
+        // http://localhost:3000/api/posts
+        this.http.delete('http://localhost:3000/api/posts/'+postId)
+        .subscribe((responseData)=>{
+            console.log('Deleted');
         });
     }
 }
