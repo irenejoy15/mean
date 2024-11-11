e141a18254461690bf2edfbe8db0b4c9c20d1fdb
Cefrian Yves Baldoza Trinchera <34486370+irenejoy15@users.noreply.github.com>
2024-11-11 15:42:29 +0800

    Sending Token to the frontend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -24,14 +24,17 @@ router.post("/signup",(req,res,next)=>{
     
 });
 
-router.post("login",(req,res,next)=>{
+router.post("/login",(req,res,next)=>{
+    let fetchUser;
     User.findOne({email:req.body.email})
         .then(user=>{
+            
             if(!user){
                 return res.status(401).json({
                     message: "Auth Failed"
                 });
             }
+            fetchUser = user;
             return bcrypt.compare(req.body.password,user.password)
         })
         .then(result=>{
@@ -40,8 +43,10 @@ router.post("login",(req,res,next)=>{
                     message: "Auth Failed"
                 });
             }
-            const token = jwt.sign({email:user.email, userId: user._id},'secret_this_should_be_longer',{expiresIn:"1h"});
-
+            const token = jwt.sign({email:fetchUser.email, userId: fetchUser._id},'secret_this_should_be_longer',{expiresIn:"1h"});
+            res.status(200).json({
+                token:token
+            });
         }).catch(err=>{
             return res.status(401).json({
                 message: "Auth Failed"
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,11 +6,22 @@ import { AuthData } from "./auth-data.model";
 export class AuthService{
     constructor(private http:HttpClient){}
     createUser(email:string,password:string){
-        //http://localhost:81/mean-backend/public/api/signup"
+        //http://localhost:82/mean-backend/public/api/signup"
+        //http://localhost:3000/api/user/signup
         const authData : AuthData = {email:email,password:password}
         this.http.post("http://localhost:3000/api/user/signup",authData)
             .subscribe(response=>{
                 console.log(response);
             });
+    }
+
+    login(email:string,password:string){
+        //http://localhost:82/mean-backend/public/api/login"
+        // http://localhost:3000/api/user/login
+        const authData : AuthData = {email:email,password:password}
+        this.http.post(" http://localhost:3000/api/user/login",authData)
+        .subscribe(response=>{
+            console.log(response);
+        });
     }
 }
