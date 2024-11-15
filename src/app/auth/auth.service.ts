94e0a353094da3d22d67f7439d9bc123d0ef3da5
Cefrian Yves Baldoza Trinchera <34486370+irenejoy15@users.noreply.github.com>
2024-11-15 16:30:34 +0800

    Local Storage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -50,9 +50,15 @@ export class AuthService{
         //     .subscribe(response=>{
         //         const token = response.token;
         //         this.token = token;
-        //          if(token){
+        //         if(token){
+            //     const expiresInDuration =response.expiresIn;
             //     this.isAuthenticated = true;
             //     this.authStatusListener.next(true);
+            //     const now = new Date();
+            //     const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+            //     console.log(expirationDate);
+            //     this.saveAuthData(token, expirationDate);
+            //     this.router.navigate(['/']);
             // }
         //     });
         // // });
@@ -62,23 +68,70 @@ export class AuthService{
             const token = response.token;
             this.token = token;
             if(token){
-                const expiresDuration =response.expiresIn;
-                this.tokenTimer = setTimeout(()=>{
-                    this.logout();
-                },expiresDuration * 1000)
+                const expiresInDuration =response.expiresIn;
+                this.setAuthTimer(expiresInDuration);
                 this.isAuthenticated = true;
                 this.authStatusListener.next(true);
+                const now = new Date();
+                const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+                console.log(expirationDate);
+                this.saveAuthData(token, expirationDate);
                 this.router.navigate(['/']);
             }
         });
     }
+
+    autoAuthUser() {
+        const authInformation = this.getAuthData();
+        if (!authInformation) {
+          return;
+        }
+        const now = new Date();
+        const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
+        if (expiresIn > 0) {
+          this.token = authInformation.token;
+          this.isAuthenticated = true;
+          this.setAuthTimer(expiresIn / 1000);
+          this.authStatusListener.next(true);
+        }
+      }
 
     logout(){
         this.token = null;
         this.isAuthenticated = false;
         this.authStatusListener.next(false);
         clearTimeout(this.tokenTimer);
+        this.clearAuthData();
         this.router.navigate(['/']);
       
+    }
+
+    private setAuthTimer(duration: number) {
+        console.log("Setting timer: " + duration);
+        this.tokenTimer = setTimeout(() => {
+          this.logout();
+        }, duration * 1000);
+      }
+
+    private saveAuthData(token: string, expirationDate: Date) {
+        localStorage.setItem("token", token);
+        localStorage.setItem("expiration", expirationDate.toISOString());
+    }
+
+    private clearAuthData() {
+        localStorage.removeItem("token");
+        localStorage.removeItem("expiration");
+      }
+    
+    private getAuthData() {
+    const token = localStorage.getItem("token");
+    const expirationDate = localStorage.getItem("expiration");
+        if (!token || !expirationDate) {
+            return 0;
+        }
+        return {
+            token: token,
+            expirationDate: new Date(expirationDate)
+        }
     }
 }
