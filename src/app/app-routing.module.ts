bc06c9446aebda41a58f5297eada4845d8600c09
Cefrian Yves Baldoza Trinchera <34486370+irenejoy15@users.noreply.github.com>
2024-11-15 15:49:26 +0800

    Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,11 @@ import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { PostEditComponent } from "./posts/post-edit/post-edit.component";
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from "./auth/signup/signup.component";
+import { AuthGuard } from "./auth/auth.guard";
 const routes : Routes= [
     {path:'', component: PostListComponent},
-    {path:'create', component: PostCreateComponent},
-    {path:'edit/:postId', component: PostCreateComponent},
+    {path:'create', component: PostCreateComponent,canActivate:[AuthGuard]},
+    {path:'edit/:postId', component: PostCreateComponent ,canActivate:[AuthGuard]},
     {path:'test/edit/:postId', component:PostEditComponent},
     {path:'login',component:LoginComponent},
     {path:'signup',component:SignupComponent}
@@ -16,7 +17,8 @@ const routes : Routes= [
 
 @NgModule({
    imports:[RouterModule.forRoot(routes)],
-   exports:[RouterModule]
+   exports:[RouterModule],
+   providers:[AuthGuard]
 })
 export class AppRoutingModule {
 
