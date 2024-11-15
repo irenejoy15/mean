cfb299fee712dd4d0cd9d8146483d18023360da8
Cefrian Yves Baldoza Trinchera <34486370+irenejoy15@users.noreply.github.com>
2024-11-15 15:29:44 +0800

    LOGOUT

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -33,54 +33,42 @@ export class AuthService{
             });
     }
 
-    // login(email:string,password:string){
-    //     //http://localhost:82/mean-backend/public/api/login"
-    //     // http://localhost:3000/api/user/login
-    //     // / http://localhost:82/mean-backend/public/api/logout
-    //     const authData : AuthData = {email:email,password:password}
+    login(email:string,password:string){
+        //http://localhost:82/mean-backend/public/api/login"
+        // http://localhost:3000/api/user/login
+        // / http://localhost:82/mean-backend/public/api/logout
+        const authData : AuthData = {email:email,password:password}
 
-    //     // axios.defaults.withCredentials = true;
-    //     // axios.defaults.withXSRFToken = true;
-    //     // const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
-    //     const headers = { 'Content-Type': 'application/json', 'My-Custom-Header': 'foobar' };
-    //     // // axios.get('http://localhost:82/mean-backend/public/sanctum/csrf-cookie',{headers}).then(response=>{
-    //     //     this.http.post<{token:string}>("http://localhost:82/mean-backend/public/api/login",authData,{headers})
-    //     //     .subscribe(response=>{
-    //     //         const token = response.token;
-    //     //         this.token = token;
-    //     //          if(token){
-    //         //     this.isAuthenticated = true;
-    //         //     this.authStatusListener.next(true);
-    //         // }
-    //     //     });
-    //     // // });
+        // axios.defaults.withCredentials = true;
+        // axios.defaults.withXSRFToken = true;
+        // const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
+        const headers = { 'Content-Type': 'application/json', 'My-Custom-Header': 'foobar' };
+        // // axios.get('http://localhost:82/mean-backend/public/sanctum/csrf-cookie',{headers}).then(response=>{
+        //     this.http.post<{token:string}>("http://localhost:82/mean-backend/public/api/login",authData,{headers})
+        //     .subscribe(response=>{
+        //         const token = response.token;
+        //         this.token = token;
+        //          if(token){
+            //     this.isAuthenticated = true;
+            //     this.authStatusListener.next(true);
+            // }
+        //     });
+        // // });
 
-    //     this.http.post<{token:string}>("http://localhost:3000/api/user/login",authData)
-    //     .subscribe(response=>{
-    //         const token = response.token;
-    //         this.token = token;
-    //         if(token){
-    //             this.isAuthenticated = true;
-    //             this.authStatusListener.next(true);
-    //         }
-    //     });
-    // }
-
-    login(email: string, password: string) {
-        const authData: AuthData = { email: email, password: password };
-        this.http
-          .post<{ token: string; expiresIn: number }>(
-            "http://localhost:3000/api/user/login",
-            authData
-          )
-          .subscribe(response => {
+        this.http.post<{token:string}>("http://localhost:3000/api/user/login",authData)
+        .subscribe(response=>{
             const token = response.token;
             this.token = token;
-            console.log(this.token);
-            if (token) {
-              this.isAuthenticated = true;
-              this.authStatusListener.next(true);
+            if(token){
+                this.isAuthenticated = true;
+                this.authStatusListener.next(true);
             }
-          });
-      }
+        });
+    }
+
+    logout(){
+        this.token = null;
+        this.isAuthenticated = false;
+        this.authStatusListener.next(false);
+    }
 }
