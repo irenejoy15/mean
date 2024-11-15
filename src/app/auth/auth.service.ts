ff3ed472b3a0f2254bd2535355bbd4bc49d37999
Cefrian Yves Baldoza Trinchera <34486370+irenejoy15@users.noreply.github.com>
2024-11-15 14:09:08 +0800

    Improving the ui messages to reflect the authentication status

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,14 +2,20 @@ import { HttpClient, HttpHeaders,HttpClientXsrfModule } from "@angular/common/ht
 import { Injectable } from "@angular/core";
 import { AuthData } from "./auth-data.model";
 import axios from 'axios';
+import { Subject } from "rxjs";
 @Injectable({providedIn:"root"})
 export class AuthService{
     private token:string;
+    private authStatusListener = new Subject<boolean>();
     constructor(private http:HttpClient){}
 
     getToken(){
         console.log(this.token);
         return this.token;
+    }
+
+    getAuthStatusListener(){
+        return this.authStatusListener.asObservable();
     }
 
     createUser(email:string,password:string){
@@ -37,6 +43,7 @@ export class AuthService{
         //     .subscribe(response=>{
         //         const token = response.token;
         //         this.token = token;
+        //          this.authStatusListener.next(true);
         //         console.log(response)
         //     });
         // // });
@@ -45,6 +52,7 @@ export class AuthService{
         .subscribe(response=>{
             const token = response.token;
             this.token = token;
+            this.authStatusListener.next(true);
         });
     }
 }
