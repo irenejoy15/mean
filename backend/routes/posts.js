bf3838974d559719d4f34f7c1a81cc50a87040f3
Cefrian Yves Baldoza Trinchera <34486370+irenejoy15@users.noreply.github.com>
2024-11-25 07:50:34 +0800

    Using COntroller

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const Post = require('../models/post')
+const PostController = require('../controller/post');
 const router = express.Router();
 const checkAuth = require("../middleware/check-auth");
 const MIME_TYPE_MAP = {
@@ -29,121 +29,22 @@ router.post(
     "",
     checkAuth,
     multer({storage:storage}).single("image"),
-    (req,res,next)=>{
-    const url = req.protocol + '://' + req.get("host");
-    const post= new Post({
-        title: req.body.title,
-        content: req.body.content,
-        imagePath: url + "/images/" + req.file.filename,
-        creator: req.userData.userId
-    });
-    post.save().then(createdPost=>{
-        res.status(201).json({
-            message: 'POST ADD SUCCESSFULLY',
-            post:{
-                id: createdPost._id,
-                title: createdPost.title,
-                content: createdPost.content,
-                imagePath:createdPost.imagePath
-            }
-        });
-    })
-    .catch(error=>{
-        return res.status(401).json({
-            message: error.toString()
-        });
-    });
-});
+    PostController.createPost
+  );
 
-router.get('',(req,res,next)=>{
-    const pageSize = +req.query.pagesize;
-    const currentPage = +req.query.page; 
-    const postQuery = Post.find();
-    let fetchedPosts;
-    if(pageSize && currentPage){
-        postQuery
-        .skip(pageSize * (currentPage-1))
-        .limit(pageSize);
-    }
-    postQuery
-    .then(documents => {
-        fetchedPosts = documents;
-        return Post.countDocuments();
-    })
-    .then(count=>{
-        res.status(200).json({
-            message: 'Post fetched sucessfully',
-            posts: fetchedPosts,
-            maxPosts: count
-        });
-    }).catch(error=>{
-        res.status(500).json({
-            message:"Fetching posts Failed!"
-        });
-    });
-});
+router.get('',PostController.getPosts);
 
-router.get("/:id",(req,res,next)=>{
-    Post.findById(req.params.id).then(post =>{
-        if(post){
-            res.status(200).json(post);
-        }else{
-            res.status(404).json({message: 'Post Not Found'});
-        }
-    }).catch(error=>{
-        res.status(500).json({
-            message:"Fetching posts Failed!"
-        });
-    });
-});
+router.get("/:id",PostController.getPost);
 
 router.put(
     "/:id",
     checkAuth,
     multer({ storage: storage }).single("image"),
-    (req, res, next) => {
-      let imagePath = req.body.imagePath;
-      if (req.file) {
-        const url = req.protocol + "://" + req.get("host");
-        imagePath = url + "/images/" + req.file.filename;
-      }
-      const post = new Post({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content,
-        imagePath: imagePath,
-        creator: req.userData.userId
-      });
-      Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
-        .then(result => {
-          if (result.nModified > 0) {
-            res.status(200).json({ message: "Update successful!" });
-          } else {
-            res.status(401).json({ message: "Not authorized!" });
-          }
-        })
-        .catch(error => {
-          res.status(500).json({
-            message: "Couldn't udpate post!"
-          });
-        });
-    }
+   PostController.updatePost
   );
 router.delete('/:id',
     checkAuth,
-    (req,res,next)=>{
-    Post.deleteOne({_id:req.params.id,creator:req.userData.userId})
-    .then(result=>{
-        // if(result.n > 0){
-            res.status(200).json({message: 'Post Deleted'});
-        // }else{
-        //     res.status(401).json({ message: "Not Authorize!" });
-        // }
-    }).catch(error=>{
-        res.status(500).json({
-            message:"Fetching posts Failed!"
-        });
-    });
-});
+    PostController.deletePost
+    );
 
 module.exports = router;
